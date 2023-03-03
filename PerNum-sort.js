bf4f52d825160030c7aa1a9eecb1d7fccbf19d2b
cinast <126345646+cinast@users.noreply.github.com>
2023-03-03 14:03:30 +0000

    -in debugging-

--- PerNum-sort.js
+++ PerNum-sort.js
@@ -1,26 +1,28 @@
 //Percent-sort(perNum-sort)
 //by coside
-function randint(min, max) { return Math.floor(Math.random() * (Math.ceil(max) - Math.min + 1)) }
+function randint(min, max) { return Math.floor(Math.random() * (Math.ceil(max) - Math.floor(min) + 1)) }
 function randomarray(item) {
     var array = []
-    for (var i = 0; i <= item; i++) {
-        arrray.spilce(randint(0, array.length), 0, i)
+    for (var i = 0; i < item; i++) {
+        array.splice(randint(0, array.length), 0, i)
     }
+    console.log(array);
     return array
 }
 function perNum(array, MaxDif) {
     var tup = [[]],
         result = [],
-        max,
         place = 0
 
     //make small groups
-    //each group's maximum and minimum difference 
+    //each tup[count]'s maximum and minimum difference 
     //will not bigger than minimum*MaxDif
-    for (var count = 0; count <= array.length; count++) {
-        for (var count2 = 0; count2 <= tup[count].length; count2++) {
-            if (array[count] / tup[count2][0] < MaxDif) {
-                //define:[0]smallest,[-1]biggest
+    for (var count = 0; count < array.length; count++) {
+        for (var count2 = 0; count2 < tup[count2] || Array.length; count2++) {
+            if (array[count] / tup[count2][0] > MaxDif && count2 >= tup[count2] || [][0]) {
+                tup.push(array[count])
+            } else {
+                //define:[0]smallest,[<length>]biggest
                 if (array[count] <= tup[count2][0]) {
                     tup[count2].unshift(array[count])
                     break
@@ -30,25 +32,28 @@ function perNum(array, MaxDif) {
                     break
                 }
             }
-            else { tup.push([array[count]]) }
-            
-            if (count2 > tum.length) {
-                tup.push([array[count]])
-            }
-            //every number will go it's percent of this small array
-            for (var count = 0; count <= tup.length; count++) {
-                //index of the small array is the smallset,last one is the biggest
-                Max = tup[count][-1]
-                for (var count2 = 0; count2 <= tup[count].length; count2++) {
-                    place = tup[conut][count2] / Max * tup[count].length
-                    if (place > count2) {
-                        place++
-                    }
-                    tup[count].splice(count2, 1)
-                }
-            }
         }
-        return result
     }
+    console.log(tup);
+    //every number will go it's percent of this small array
+    for (var count = 0; count <= tup.length; count++) {
+        //index of the small array is the smallset,last one is the biggest
+        var Max = tup[count].slice(-1, -1),
+            item = tup[count][count2]
+        for (var count2 = 0; count2 <= tup[count].length; count2++) {
+            place = item / Max * tup[count].length
+            if (place > count2) {
+                place++
+            }
+            tup[count].splice(count2, 1)
+            tup[count].splice(place, 0, item)
+        }
+    }
+
+    for (var count = 0; count <= tup.length; count++) {
+        result += tup[count]
+    }
+    return result
+
 }
-console.log(perNum(randomarray(),5))
+console.log(perNum(randomarray(100), 5))
